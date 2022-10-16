9c489e00978c34b6ddf0bcc931b74bb789fc818a
Khandaker Md. Saiful Islam <91514767+Nafees37@users.noreply.github.com>
2022-10-16 15:32:48 +0600

    Add files via upload

---
+++ click change image javascript/custom.js
@@ -0,0 +1,17 @@
+let parentImage = document.getElementById("first_image");
+
+function myImageFun_1() {
+    parentImage.src = "images/2.jpg";
+}
+
+function myImageFun_2() {
+    parentImage.src = "images/3.jpg";
+}
+
+function myImageFun_3() {
+    parentImage.src = "images/4.jpg";
+}
+
+function myImageFun_4() {
+    parentImage.src = "images/5.jpg";
+}
